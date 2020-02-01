1d26f7b603fd2752d60101cf2ee4fcca83329123
PolMesegue <pol.mesegue@est.fib.upc.edu>
2020-02-01 18:16:45 +0100

    afegint try-catch-finally

--- PortadesDelPassat2.py
+++ PortadesDelPassat2.py
@@ -9,67 +9,78 @@ from datetime import datetime
 from dateutil.relativedelta import relativedelta
 from pdf2image import convert_from_path
 
-keys_file = open("../keys.txt")
-lines = keys_file.readlines()
-CONSUMER_KEY = lines[0].rstrip()
-CONSUMER_SECRET = lines[1].rstrip()
-ACCESS_KEY = lines[2].rstrip()
-ACCESS_SECRET = lines[3].rstrip()
+try:
 
-auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
-auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
+	os.mkdir('temporal')
 
-api = tweepy.API(auth)
+	keys_file = open("keys.txt")
+	lines = keys_file.readlines()
+	CONSUMER_KEY = lines[0].rstrip()
+	CONSUMER_SECRET = lines[1].rstrip()
+	ACCESS_KEY = lines[2].rstrip()
+	ACCESS_SECRET = lines[3].rstrip()
+	keys_file.close()
 
-os.mkdir('./temporal')
+	auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+	auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
 
-#Time relative work
+	api = tweepy.API(auth)
 
-years_elapsed=int(sys.argv[1])
-year_ago = datetime.now() - relativedelta(years=years_elapsed)
-year = year_ago.strftime("%Y")
-lc_year = year_ago.strftime("%y")
-month = year_ago.strftime("%m")
-day = year_ago.strftime("%d")
+	#Time relative work
 
-aux=['E','F','M','A','Y','J','L','G','S','O','N','D']
-letter_month=aux[int(month)-1]
+	years_elapsed=int(sys.argv[1])
+	year_ago = datetime.now() - relativedelta(years=years_elapsed)
+	year = year_ago.strftime("%Y")
+	lc_year = year_ago.strftime("%y")
+	month = year_ago.strftime("%m")
+	day = year_ago.strftime("%d")
 
-#LaVanguardia
+	aux=['E','F','M','A','Y','J','L','G','S','O','N','D']
+	letter_month=aux[int(month)-1]
 
-url = f'http://hemeroteca-paginas.lavanguardia.com/LVE05/PUB/{year}/{month}/{day}/LVG{year}{month}{day}0011LB.pdf'
-wget.download(url, './temporal/lavanguardia.pdf')
+	#LaVanguardia
 
-pages = convert_from_path('./temporal/lavanguardia.pdf', 500)
+	url = f'http://hemeroteca-paginas.lavanguardia.com/LVE05/PUB/{year}/{month}/{day}/LVG{year}{month}{day}0011LB.pdf'
+	wget.download(url, 'temporal/lavanguardia.pdf')
 
-for page in pages:
-    page.save('./temporal/lavanguardia.jpg', 'JPEG')
+	pages = convert_from_path('temporal/lavanguardia.pdf', 500)
 
-#ABC
+	for page in pages:
+	    page.save('temporal/lavanguardia.jpg', 'JPEG')
 
-url = f'http://hemeroteca.abc.es/cgi-bin/pagina.pdf?fn=exec;command=stamp;path=H:\cran\data\prensa_pages\Madrid\ABC\{year}\{year}{month}\{year}{month}{day}\{lc_year}{letter_month}{day}-001.xml;id=0006839617#view=Fit'
-wget.download(url, './temporal/abc.pdf')
-pages = convert_from_path('./temporal/abc.pdf', 500)
+	#ABC
 
-for page in pages:
-    page.save('./temporal/abc.jpg', 'JPEG')
+	url = f'http://hemeroteca.abc.es/cgi-bin/pagina.pdf?fn=exec;command=stamp;path=H:\cran\data\prensa_pages\Madrid\ABC\{year}\{year}{month}\{year}{month}{day}\{lc_year}{letter_month}{day}-001.xml;id=0006839617#view=Fit'
+	wget.download(url, 'temporal/abc.pdf')
+	pages = convert_from_path('temporal/abc.pdf', 500)
 
-#ElPais
+	for page in pages:
+	    page.save('temporal/abc.jpg', 'JPEG')
 
-url = f'https://srv00.epimg.net/pdf/elpais/snapshot/{year}/{month}/elpais/{year}{month}{day}Big.jpg'
-wget.download(url, './temporal/elpais.jpg')
+	#ElPais
+
+	url = f'https://srv00.epimg.net/pdf/elpais/snapshot/{year}/{month}/elpais/{year}{month}{day}Big.jpg'
+	wget.download(url, 'temporal/elpais.jpg')
 
 
-filenames = ['./temporal/elpais.jpg', './temporal/lavanguardia.jpg', './temporal/abc.jpg']
-uploaded_ids = []
+	filenames = ['temporal/elpais.jpg', 'temporal/lavanguardia.jpg', 'temporal/abc.jpg']
+	uploaded_ids = []
 
-for filename in filenames:
-	res = api.media_upload(filename)
+	for filename in filenames:
+		res = api.media_upload(filename)
+		uploaded_ids.append(res.media_id)
+
+	anyoanys = 'any' if years_elapsed == 1 else 'anys'
+
+	api.update_status(status= f'Portades dels principals diaris {years_elapsed} {anyoanys} enrere, dia {day}/{month}/{year}', media_ids=uploaded_ids)
+
+except:
+
+	uploaded_ids = []
+	res = api.media_upload('fail.jpg')
 	uploaded_ids.append(res.media_id)
+	api.update_status("Per motius tècnics, no ha sigut possible la piulada, ho intentarem de nou en breus instants", media_ids=uploaded_ids)
 
-anyoanys='any' if years_elapsed==1 else 'anys'
+finally:
 
-api.update_status(status= f'Portades dels principals diaris {years_elapsed} {anyoanys} enrere, dia {day}/{month}/{year}', media_ids=uploaded_ids)
-
-shutil.rmtree('./temporal')
-
+	shutil.rmtree('temporal')
